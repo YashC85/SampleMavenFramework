1dfdc54b3a8fc907e5666f248717dd971a9a5710
PC <PC@PC>
2021-07-15 00:01:21 +0530

    new organisation test

---
+++ SampleMaven_Framework/src/test/java/com/crm/vtiger/organisation/CreateOrgTest.java
@@ -0,0 +1,29 @@
+package com.crm.vtiger.organisation;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CreateOrgTest {
+	@Test
+	public void createOrgTest() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("http://localhost:8888");
+		driver.findElement(By.name("user_name")).sendKeys("admin");
+		driver.findElement(By.name("user_password")).sendKeys("admin", Keys.ENTER);
+		
+		System.out.println(driver.getTitle());
+		driver.close();
+		
+		
+	}
+
+}
