26f51fcb58899aba78be33204c0192b5fe93910f
PC <PC@PC>
2021-07-15 22:51:34 +0530

    new push

--- SampleMaven_Framework/src/test/java/com/crm/vtiger/organisation/CreateOrgTest.java
+++ SampleMaven_Framework/src/test/java/com/crm/vtiger/organisation/CreateOrgTest.java
@@ -7,15 +7,30 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class CreateOrgTest {
-	@Test(groups = "smokeTest")
+	@Test
 	public void createOrgTest() {
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
+		WebDriver driver = null;
+		
+		String browser = System.getProperty("BROWSER");
+		if(browser.equals("chrome"))
+		{
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}
+		else if(browser.equals("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		}
+		else {
+			System.out.println("Provide valid browser name");
+		}
+				
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get("http://localhost:8888");
 		driver.findElement(By.name("user_name")).sendKeys("admin");
